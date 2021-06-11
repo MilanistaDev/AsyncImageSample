ea99e142bfa65b8da43c7ab3cffbb0ad3886c2bb
Takuya Aso <milanista224@gmail.com>
2021-06-11 23:57:40 +0900

    Use Placeholder and customize Image.

--- AsyncImageSample/ContentView.swift
+++ AsyncImageSample/ContentView.swift
@@ -14,7 +14,20 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             VStack {
-                AsyncImage(url: URL(string: profileImageLink))
+                AsyncImage(url: URL(string: profileImageLink)) { image in
+                    image
+                        .resizable()
+                        .scaledToFit()
+                        .cornerRadius(12.0)
+                        .frame(width: 200.0, height: 200.0)
+                } placeholder: {
+                    ProgressView("Loading")
+                        .frame(width: 80.0, height: 80.0)
+                        .background(Color.gray).opacity(1)
+                        .cornerRadius(8.0)
+                }
+                .padding(.top, 50.0)
+                Spacer()
             }
             .navigationTitle("AsyncImage Sample")
         }
