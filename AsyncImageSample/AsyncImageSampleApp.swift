0c643fd7b6fcea1e8f7c5cde1f09660b03868b62
Takuya Aso <milanista224@gmail.com>
2021-06-11 21:27:58 +0900

    Initial Commit

---
+++ AsyncImageSample/AsyncImageSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageSampleApp.swift
+//  AsyncImageSample
+//
+//  Created by Takuya Aso on 2021/06/11.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
